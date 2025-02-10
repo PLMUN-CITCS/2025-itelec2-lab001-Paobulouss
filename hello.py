2479c4738709b23e0a7ab14b37f0825a65124599
Christian Paolo Marfil <marfilchristianpaolo_bsit@plmun.edu.ph>
2025-02-10 09:24:47 +0800

    hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Hello world!")
-print("Name: [Christian Paolo Marfil]")
-print("Course: [Information Technology]")
-print("Interests in programming: [coding in c++]")
+print("Name: Christian Paolo Marfil")
+print("Course: Information Technology")
+print("Interests in programming: Web Designing")
